b96adc276f46d5b7d7bff87114c52058aa25a364
omerbackflip <31404094+omerbackflip@users.noreply.github.com>
2018-09-15 13:28:32 +0300

    feat () : node server
    Run sporty app in node server in the right way to displa the right images and css stylesheets

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+ 
+ //use the application off of express.
+ var app = express();
+ 
+app.use(express.static(__dirname)); //Serves resources from public folder
+
+
+ //define the route for "/"
+ app.get("/", function (request, response){
+     //show this file when the "/" is requested
+     response.sendFile(__dirname+"/main.html");
+ });
+ 
+ //start the server
+ app.listen(8080);
+ 
+ console.log("Something awesome to happen at http://localhost:8080");
