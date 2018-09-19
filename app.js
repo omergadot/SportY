82f7432a12a91e1e59ecbb624ada2dafb0d25620
omergadot <omer.gadot1@gmail.com>
2018-09-19 09:27:16 +0300

    fix changes

--- app.js
+++ app.js
@@ -14,12 +14,7 @@ app.use(express.static(__dirname)); //Serves resources from public folder
  });
  
  //start the server
-<<<<<<< HEAD
- app.listen(3457);
- 
- console.log("Something awesome to happen at http://localhost:3457");
-=======
  app.listen(portNum);
  
  console.log("Server is live and running at port : " + portNum);
->>>>>>> cc9dbfde230d3eac0e9ea9a6779f95b1d3f1f274
+
